59511b2beab35d252a99f7c13d9f1f3b4ea4a781
Johnny Martinez <mrtnz.jn@gmail.com>
2023-02-14 15:57:07 -0600

    modified HelloWorldServlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,10 +8,18 @@ import java.io.PrintWriter;
 @WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1> Hello, World! and Mars </h1>");
+        String userName = request.getParameter("name");
+        if (userName == null) {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h1> Hello, World! and Mars </h1>");
+        } else {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h1> Hello, </h1>" + userName + "<h1>and Mars </h1>");
+        }
     }
 }
