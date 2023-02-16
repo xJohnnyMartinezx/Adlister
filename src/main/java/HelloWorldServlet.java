a778148527287d556bd7ca90fe723325b6362a5d
Johnny <mrtnz.jn@gmail.com>
2023-02-15 20:55:06 -0600

    Refactored... Removed common references from if statement.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -12,16 +12,31 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         String userName = request.getParameter("name");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
         if (userName == null) {
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
             out.println("<h1> Hello, World! and Mars </h1>");
         } else {
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
             out.println("<h1> Hello, </h1>" + userName + "<h1>and Mars </h1>");
         }
     }
+
+
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+//        String userName = request.getParameter("name");
+//        if (userName == null) {
+//            response.setContentType("text/html");
+//            PrintWriter out = response.getWriter();
+//            out.println("<h1> Hello, World! and Mars </h1>");
+//        } else {
+//            response.setContentType("text/html");
+//            PrintWriter out = response.getWriter();
+//            out.println("<h1> Hello, </h1>" + userName + "<h1>and Mars </h1>");
+//        }
+//    }
 }
+
+
 
 //This is a test comment
