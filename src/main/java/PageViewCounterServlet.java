5d46ba122b1aefa6b906eae4aa9984b74a732d9a
Johnny <mrtnz.jn@gmail.com>
2023-02-15 20:39:47 -0600

    Created PageViewCounterServlet and successfully tested it

---
+++ src/main/java/PageViewCounterServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name="PageViewCounterServlet", urlPatterns = "/count")
+public class PageViewCounterServlet extends HttpServlet {
+
+    private int pageCount = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String reset = request.getParameter("reset");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if (reset != null){
+            pageCount = 0;
+        } else {
+            pageCount++;
+        }
+        out.println("<h4> Page views: " + pageCount + "</h4>");
+    }
+
+}
